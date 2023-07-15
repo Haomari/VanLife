0749372188faa2cd09aaa5cd3de0593e6a306869
Haomari <tcurpal@gmail.com>
2023-07-15 13:22:36 +0200

    Initialize project using Create React App

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./sections/Header";
+import Footer from "./sections/Footer";
+
+function App() {
+  return (
+		<>
+			<Header />
+			<main className="page">
+			
+			</main>
+			<Footer />
+		</>
+  );
+}
+
+export default App;
