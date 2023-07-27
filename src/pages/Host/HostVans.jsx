dcd51f084bde8d09bd088e1100fe3a3004f1825d
Haomari <tcurpal@gmail.com>
2023-07-27 19:10:36 +0200

    HostVans added

---
+++ src/pages/Host/HostVans.jsx
@@ -0,0 +1,14 @@
+import HostVanList from "../../app-components/HostVanList";
+
+export default function HostVans() {
+  return (
+    <section className="host__vans vans-host">
+      <div className="vans-host__container">
+        <h2 className="vans-host__title">Your listed vans</h2>
+        <div className="vans-host__vans-list vans-list">
+          <HostVanList />
+        </div>
+      </div>
+    </section>
+  );
+}
