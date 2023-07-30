050d7d70938a5c75208c70e5f6e674b77f2aa18c
Haomari <tcurpal@gmail.com>
2023-07-30 18:25:22 +0200

    started building HostIncome

---
+++ src/app-components/host/HostDashboard.jsx
@@ -0,0 +1,5 @@
+export default function HostDashboard() {
+	return (
+		<div className="dashboard-host__body"></div>
+	)
+}
---
+++ src/pages/Host/HostIncome.jsx
@@ -0,0 +1,27 @@
+import HostDashboard from "../../app-components/host/HostDashboard";
+
+export default function Income() {
+  return (
+    <section className="host__income income-host">
+      <div className="income-host__container">
+        <div className="income-host__income-body">
+          <h2 className="income-host__title">Income</h2>
+          <div className="income-host__time">
+            <p>
+              Last <span>30 days</span>
+            </p>
+          </div>
+          <div className="income-host__amount">
+            <p>$2,260</p>
+          </div>
+          <div className="income-host__dashboard dashboard-host">
+            <HostDashboard />
+          </div>
+        </div>
+				<div className="income-host__transaction transaction-host">
+
+				</div>
+      </div>
+    </section>
+  );
+}
