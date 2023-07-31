7131e0c8cd7a54addeab9e086669f8a373bcfbaa
Haomari <tcurpal@gmail.com>
2023-07-31 20:05:04 +0200

    NavLink added

--- src/app-components/Header.jsx
+++ src/app-components/Header.jsx
@@ -1,6 +1,5 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logo from "../img/logo.png";
-
 
 export default function Header() {
   return (
@@ -9,11 +8,38 @@ export default function Header() {
         <Link to={"/"}>
           <img src={logo} className="header__logo"></img>
         </Link>
-				<nav className="header__menu">
-					<Link to={"/host"} className="header__menu-item">Host</Link>
-					<Link to={"/about"} className="header__menu-item">About</Link>
-					<Link to={"/vans"} className="header__menu-item">Vans</Link>
-				</nav>
+        <nav className="header__menu">
+          <NavLink
+            to={"/host"}
+            className={({ isActive }) =>
+              isActive
+                ? "header__menu-item header__menu-item--selected"
+                : "header__menu-item"
+            }
+          >
+            Host
+          </NavLink>
+          <NavLink
+            to={"/about"}
+            className={({ isActive }) =>
+              isActive
+                ? "header__menu-item header__menu-item--selected"
+                : "header__menu-item"
+            }
+          >
+            About
+          </NavLink>
+          <NavLink
+            to={"/vans"}
+            className={({ isActive }) =>
+              isActive
+                ? "header__menu-item header__menu-item--selected"
+                : "header__menu-item"
+            }
+          >
+            Vans
+          </NavLink>
+        </nav>
       </div>
     </header>
   );
--- src/app-components/host/HostLayout.jsx
+++ src/app-components/host/HostLayout.jsx
@@ -1,23 +1,43 @@
 import { Outlet } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function HostLayout() {
   return (
     <>
       <main className="host">
         <nav className="host__host-menu host-menu host-menu__container">
-          <Link className="host-menu__link" to={"/host"}>
+          <NavLink
+            className={({ isActive }) =>
+              isActive
+                ? "host-menu__link host-menu__link--selected"
+                : "host-menu__link"
+            }
+						end
+            to={"/host"}
+          >
             Dashboard
-          </Link>
-          <Link className="host-menu__link" to={"/host/income"}>
+          </NavLink>
+          <NavLink className={({ isActive }) =>
+              isActive
+                ? "host-menu__link host-menu__link--selected"
+                : "host-menu__link"
+            } to={"/host/income"}>
             Income
-          </Link>
-          <Link className="host-menu__link" to={"/host/vans"}>
+          </NavLink>
+          <NavLink className={({ isActive }) =>
+              isActive
+                ? "host-menu__link host-menu__link--selected"
+                : "host-menu__link"
+            } to={"/host/vans"}>
             Vans
-          </Link>
-          <Link className="host-menu__link" to={"/host/reviews"}>
+          </NavLink>
+          <NavLink className={({ isActive }) =>
+              isActive
+                ? "host-menu__link host-menu__link--selected"
+                : "host-menu__link"
+            } to={"/host/reviews"}>
             Reviews
-          </Link>
+          </NavLink>
         </nav>
         <Outlet />
       </main>
